d9119d3a5951828d93cd28b463e617ddf4529ac0
Ha Minh <minhha@viajsc.onmircrosoft.com>
2024-09-01 21:51:56 +0700

    update requirements

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ def change_password():
 
 
 @app.route("/syncPodbeanData", methods=["GET"])
-# @jwt_required()
+@jwt_required()
 def sync_podbean_data():
     auth = ("890c52f9d202851c9ba76", "6b61edbd548dda391f100")
     data = {"grant_type": "client_credentials"}
